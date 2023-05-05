b5bc6e25eee3967d68d05fe97d7d8d89ab1a443f
Elvira Magasumova <mova.es94@gmail.com>
2023-05-06 00:47:30 +0500

    lesson 3 complete and career mobile done

--- src/index.js
+++ src/index.js
@@ -1,13 +1,42 @@
-import './index.html';
+import "./index.html";
 //new modules
-import 'swiper/scss';
-import 'swiper/scss/pagination';
-import './index.scss';
-
+import "swiper/scss";
+import "swiper/scss/pagination";
+import "./index.scss";
+import { slidersInit } from "./modules/sliders";
 
 //use modules
 
-const videoBG = document.querySelector('.video-bg');
+slidersInit(".career__slider", {
+  pagination: {
+    el: ".career__slider-pagination",
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    1240: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
+});
+
+slidersInit(".about__slider", {
+  pagination: {
+    el: ".about__slider-pagination",
+  },
+});
+
+const videoBG = document.querySelector(".video-bg");
 
 videoBG.innerHTML = `
 <source src="video/video.webm" type="video/webm">
